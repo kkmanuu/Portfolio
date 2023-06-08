ae7b6e8d1e3bb3de2ef2417aff72a5fbf2c93d97
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-06-08 15:49:19 +0300

    Add Local-storage , Mobile-Menu,Portfolio-deployment, Animation-and-transition and Desktop

---
+++ script.js
@@ -0,0 +1,73 @@
+
+const bodyEl = document.querySelector('body');
+const closeBtn = document.querySelector('.close-icon');
+const navBtn = document.querySelector('.nav-btn');
+const navEl = document.querySelector('.navbar');
+const navItems = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-link');
+const projectLogo = document.querySelector('.nav-logo');
+function operations() {
+  closeBtn.classList.toggle('hidden');
+  navBtn.classList.toggle('hidden');
+  navEl.classList.toggle('nav-mobile');
+  bodyEl.classList.toggle('overflow');
+  projectLogo.classList.toggle('opacity');
+  navItems.classList.toggle('mobile-nav-list');
+}
+
+navBtn.addEventListener('click', operations);
+closeBtn.addEventListener('click', operations);
+navLinks.forEach((element) => {
+  element.addEventListener('click', () => {
+    bodyEl.classList.remove('overflow');
+    navEl.classList.remove('nav-mobile');
+    navItems.classList.remove('mobile-nav-list');
+    closeBtn.classList.add('hidden');
+    navBtn.classList.remove('hidden');
+    projectLogo.classList.remove('opacity');
+  });
+});
+
+
+const error = document.getElementById('error-message');
+const form = document.querySelector('.contact-form');
+const userEmail = document.getElementById('user-email');
+const errorMessage = 'Please write your email address in lower case';
+form.addEventListener('submit', (event) => {
+
+  event.preventDefault();
+  const userInput = userEmail.value;
+  if (userInput !== userInput.toLowerCase()) {
+    error.innerText = errorMessage;
+  } else {
+    error.innerText = '';
+    form.submit();
+  }
+});
+
+//  STORAGE
+const userName = document.getElementById('user-name');
+const userMessage = document.getElementById('user-message');
+const contactDetails = {
+  fullName: String,
+  email: String,
+  message: String,
+};
+
+form.addEventListener('input', () => {
+  contactDetails.fullName = userName.value;
+  contactDetails.email = userEmail.value;
+  contactDetails.message = userMessage.value;
+
+  localStorage.setItem('contactDetails', JSON.stringify(contactDetails));
+});
+
+window.onload = () => {
+  const contactData = JSON.parse(localStorage.getItem('contactDetails'));
+
+  if (contactData) {
+    userName.value = contactData.fullName;
+    userEmail.value = contactData.email;
+    userMessage.value = contactData.message;
+  }
+};
